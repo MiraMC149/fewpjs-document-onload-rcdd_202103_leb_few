f72dd19c2c4ac4cd90843cea179eda08c32a8e69
Mira Chami <miramc149@gmail.com>
2021-04-16 10:07:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Your code goes here
 
 document.addEventListener("DOMContentLoaded", function() {
-myScript();
-
+console.log("The DOM has loaded");
 });
+myScript();
 function myScript(){
   let text = document.getElementById("text");
   text.innerHTML.replace ("This is really cool!");
