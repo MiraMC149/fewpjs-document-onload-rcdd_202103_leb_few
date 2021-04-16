52527e755194322d5863b9715e312a5a015f122a
Mira Chami <miramc149@gmail.com>
2021-04-16 09:52:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Your code goes here
 let text = document.getElementById("text");
-document.getElementById("text").addEventListener("DOMContentLoaded", event);
+text.addEventListener("DOMContentLoaded", event());
 function event(){
   text.innerHTML= "This is really cool!";
 }
