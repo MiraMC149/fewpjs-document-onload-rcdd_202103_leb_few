e7f268d39264ea16548db36bb0e13197ae13b4f0
Mira Chami <miramc149@gmail.com>
2021-04-16 09:57:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Your code goes here
 let text = document.getElementById("text");
-text.addEventListener("DOMContentLoaded", event());
+text.addEventListener("DOMContentLoaded", event(){
 function event(){
   text.innerHTML.replace ("This is really cool!");
+}
 }
