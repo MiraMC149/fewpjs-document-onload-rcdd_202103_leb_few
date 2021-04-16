13aa1a9a0ca95028739c16d66d55e30b12236b5c
Mira Chami <miramc149@gmail.com>
2021-04-16 10:04:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // Your code goes here
 
 document.addEventListener("DOMContentLoaded", function() {
+myScript();
+
+});
+function myScript(){
   let text = document.getElementById("text");
   text.innerHTML.replace ("This is really cool!");
-});
+}
